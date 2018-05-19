ebdae1df33e24fcfcdc107ce08a5038dfdbbeb6d
Emily Jirles <emily.jirles@gmail.com>
2018-05-19 14:22:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -5,18 +5,22 @@ class Application
     req = Rack::Request.new 
     
     if req.path.match(/items/)
-      item = req.path.split('/items/').last 
+      item = req.path.split('/items/').last
+      
       if @@items.include?(item)
         resp.write item.price
         resp.status = 200
+        
       else 
         resp.write "Item not found"
         resp.status = 400
       end 
+      
     else
       resp.write "Route not found"
       resp.status = 404 
     end 
+    
   end 
   
 end 
