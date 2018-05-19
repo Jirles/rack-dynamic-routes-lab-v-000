aeb752efde786d9d4513e83c7bf644f35a88b964
Emily Jirles <emily.jirles@gmail.com>
2018-05-19 14:22:09 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -12,10 +12,11 @@ class Application
       else 
         resp.write "Item not found"
         resp.status = 400
-      
+      end 
     else
       resp.write "Route not found"
       resp.status = 404 
+    end 
   end 
   
 end 
