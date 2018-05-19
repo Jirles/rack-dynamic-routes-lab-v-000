26e25c1b2a4b8a21e3c16a986463fee429c9c561
Emily Jirles <emily.jirles@gmail.com>
2018-05-19 14:42:24 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -4,7 +4,7 @@ class Application
     resp = Rack::Response.new 
     req = Rack::Request.new(env) 
     
-    if req.path.match(/items/)
+    if req.path == "/items"
       item = req.path.split('/items/').last
       
       if item_available(item)
