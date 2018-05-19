f5a672d564e8760a17991bde79838c6103a398fd
Emily Jirles <emily.jirles@gmail.com>
2018-05-19 14:18:38 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,14 @@
 class Application
   
   def call(env)
+    resp = Rack::Response.new 
+    req = Rack::Request.new 
     
+    if req.path.match(/items/)
+      
+    else
+      resp.write "We don't have that item"
+      resp.status = 404 
   end 
   
 end 
