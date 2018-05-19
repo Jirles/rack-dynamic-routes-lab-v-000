86e56fda1ea91c018c515f9343ee0bc95914426d
Emily Jirles <emily.jirles@gmail.com>
2018-05-19 14:22:02 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,8 +8,10 @@ class Application
       item = req.path.split('/items/').last 
       if @@items.include?(item)
         resp.write item.price
+        resp.status = 200
       else 
-        resp.write ""
+        resp.write "Item not found"
+        resp.status = 400
       
     else
       resp.write "Route not found"
